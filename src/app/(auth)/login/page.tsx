bf9c2a8b1c6fa793179ba3e7d0aea08609a0222f
DonTeejay01 <donteejay01@gmail.com>
2025-04-23 17:40:00 +0100

    Revert "Feat: Authentication Pages(Signup, Login & Forgot Password)"
    This reverts commit aec16df7a4556e34f26a6d460c881d414590ff6e.

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,28 +1,3 @@
-import Link from "next/link";
-
-export default function Page() {
-  return          <div className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-md space-y-6 z-40">
-         <h2 className="text-2xl font-bold text-center text-green-700">Welcome Back 👋</h2>
-    
-    <form className="space-y-4">
-  <div>
-    <label>Email</label>
-    <input type="email" className="w-full border rounded p-2 mt-1" placeholder="you@example.com" />
-  </div>
-  <div>
-    <label>Password</label>
-    <input type="password" className="w-full border rounded p-2 mt-1" placeholder="••••••••" />
-  </div>
-  <div className="flex justify-between items-center text-sm">
-    <Link href="/forgot-password" className="text-green-700 hover:underline">
-      Forgot password?
-    </Link>
-  </div>
-  <button className="w-full bg-green-600 text-white py-2 rounded hover:bg-green-700 transition">
-    Login
-  </button>
-  <p className="text-sm text-center">
-    Don’t have an account? <Link href="/signup" className="text-green-700 hover:underline">Sign up</Link>
-  </p>
-</form></div>;
+export default function Login() {
+  return <div>Login</div>;
 }
